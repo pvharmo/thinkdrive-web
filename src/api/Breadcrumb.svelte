affd10b238e07b8ee790dcc6687fdf7f4b483604
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2021-08-03 10:18:27 -0400

    Fix breadcrumb

--- src/api/Breadcrumb.svelte
+++ src/api/Breadcrumb.svelte
@@ -7,12 +7,12 @@
   $: folders = $location.split('/').slice(1, -1)
 
   const go = (i: number) => {
-    const path = '/' + folders.slice(0, i).join()
+    const path = '/' + folders.slice(0, i).join() + '/'
     push(path)
   }
 </script>
 
-<Button on:click={() => go(0)}>ThinkDrive</Button>
+<Button on:click={() => push('/')}>ThinkDrive</Button>
 {#each folders as folder, i}
   <Icon icon={mdiChevronRight} /><Button on:click={() => go(i + 1)}>{folder}</Button>
 {/each}
