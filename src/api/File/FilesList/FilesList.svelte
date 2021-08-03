06dd5f8cd2bced516791189493e463810231c69d
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2021-08-03 10:12:57 -0400

    Show text if list is empty

--- src/api/File/FilesList/FilesList.svelte
+++ src/api/File/FilesList/FilesList.svelte
@@ -3,39 +3,55 @@
   import TableRow from 'components/Material/Table/TableRow.svelte'
   import TableHead from 'components/Material/Table/TableHead.svelte'
   import FileRow from './FileRow.svelte'
-  import type { File } from 'src/api/File/File'
+  import type { FileData } from 'src/api/File/File'
   import { createEventDispatcher } from 'svelte'
 
-  export let rows: File[]
-  export let selected: File[] = []
+  export let rows: FileData[]
+  export let selected: FileData[] = []
 
   const dispatch = createEventDispatcher()
 </script>
 
-<Table>
-  <thead>
-    <TableRow>
-      <TableHead>
-        <h3>Name</h3>
-      </TableHead>
-      <TableHead>
-        <h3>Location</h3>
-      </TableHead>
-      <TableHead>
-        <h3>Owner</h3>
-      </TableHead>
-      <TableHead>
-        <h3>Last modified</h3>
-      </TableHead>
-    </TableRow>
-  </thead>
-  <tbody>
-    {#each rows as row}
-      <FileRow
-        {row}
-        selected={selected.includes(row)}
-        on:click={() => dispatch('select', { value: row })}
-      />
-    {/each}
-  </tbody>
-</Table>
+{#if rows.length > 0}
+  <Table>
+    <thead>
+      <TableRow>
+        <TableHead>
+          <h3>Name</h3>
+        </TableHead>
+        <TableHead>
+          <h3>Location</h3>
+        </TableHead>
+        <TableHead>
+          <h3>Owner</h3>
+        </TableHead>
+        <TableHead>
+          <h3>Last modified</h3>
+        </TableHead>
+      </TableRow>
+    </thead>
+    <tbody>
+      {#each rows as row}
+        <FileRow
+          {row}
+          selected={selected.includes(row)}
+          on:click={() => dispatch('select', { value: row })}
+        />
+      {/each}
+    </tbody>
+  </Table>
+{:else}
+  <div class="empty-list">
+    <h2 class="empty-list">No Files</h2>
+  </div>
+{/if}
+
+<style>
+  div.empty-list {
+    margin-left: 10px;
+  }
+
+  h2.empty-list {
+    color: rgba(0, 0, 0, 0.2);
+  }
+</style>
--- src/api/Folder/FoldersList/FoldersList.svelte
+++ src/api/Folder/FoldersList/FoldersList.svelte
@@ -10,16 +10,32 @@
   const dispatch = createEventDispatcher()
 </script>
 
-<Grid container>
-  {#each folders as folder}
-    <Grid>
-      <FolderComponent
-        selected={selected.includes(folder)}
-        on:click={() => dispatch('select', { value: folder })}
-        on:dblclick={() => dispatch('goto', { value: folder.location + folder.name + '/' })}
-      >
-        {folder.name}
-      </FolderComponent>
-    </Grid>
-  {/each}
-</Grid>
+{#if folders.length > 0}
+  <Grid container>
+    {#each folders as folder}
+      <Grid>
+        <FolderComponent
+          selected={selected.includes(folder)}
+          on:click={() => dispatch('select', { value: folder })}
+          on:dblclick={() => dispatch('goto', { value: folder.location + folder.name + '/' })}
+        >
+          {folder.name}
+        </FolderComponent>
+      </Grid>
+    {/each}
+  </Grid>
+{:else}
+  <div class="empty-list">
+    <h2 class="empty-list">No folders</h2>
+  </div>
+{/if}
+
+<style>
+  div.empty-list {
+    margin-left: 10px;
+  }
+
+  h2.empty-list {
+    color: rgba(2, 1, 1, 0.2);
+  }
+</style>
